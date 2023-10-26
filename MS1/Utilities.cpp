4ffd459db2aa35300826ccdba8783e326ea0f8a2
Clara Battesini <clarabattesini@gmail.com>
2023-10-26 08:19:06 -0400

    feat: m_widthField getter and setter

--- MS1/Utilities.cpp
+++ MS1/Utilities.cpp
@@ -11,5 +11,26 @@
 
 namespace sdds
 {
+    void Utilities::setFieldWidth(size_t newWidth)
+    {
+        m_widthField = newWidth;
+    };
+
+    size_t Utilities::getFieldWidth() const
+    {
+        return m_widthField;
+    };
+
+    std::string Utilities::extractToken(const std::string &str, size_t &next_pos, bool &more){
+
+    };
+
+    void Utilities::setDelimiter(char newDelimiter){
+
+    };
+
+    char Utilities::getDelimiter(){
+
+    };
 
 }
