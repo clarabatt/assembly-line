d9e30395988d56b6056ca37c9ffeb3a84bb98810
Clara Battesini <clarabattesini@gmail.com>
2023-10-26 07:52:50 -0400

    Feat: setting up utilities and station classes

---
+++ MS1/Station.h
@@ -0,0 +1,19 @@
+// *****************************************************************************
+// Author  : Clara Verena Brito Battesini
+// Student ID(s)#: 143430213
+// Email        : cverena-brito-battes@senecacollege.ca
+// 26/10/2023
+//
+// I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+// *****************************************************************************
+
+#ifndef SDDS_STATION_H
+#define SDDS_STATION_H
+
+#include <iomanip>
+#include <iostream>
+
+namespace sdds
+{
+}
+#endif
---
+++ MS1/Utilities.cpp
@@ -0,0 +1,15 @@
+// *****************************************************************************
+// Author  : Clara Verena Brito Battesini
+// Student ID(s)#: 143430213
+// Email        : cverena-brito-battes@senecacollege.ca
+// 26/10/2023
+//
+// I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+// *****************************************************************************
+
+#include "Utilities.h"
+
+namespace sdds
+{
+
+}
