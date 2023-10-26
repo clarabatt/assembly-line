9417ee22491e814d7337a5041c8db9db016db9eb
Clara Battesini <clarabattesini@gmail.com>
2023-10-26 07:57:32 -0400

    feat: utilities header file

--- MS1/Utilities.h
+++ MS1/Utilities.h
@@ -15,5 +15,18 @@
 
 namespace sdds
 {
+    class Utilities
+    {
+        size_t m_widthField = 1;
+        static std::string m_delimiter;
+
+    public:
+        void setFieldWidth(size_t newWidth);
+        size_t getFieldWidth() const;
+        std::string extractToken(const std::string &str, size_t &next_pos, bool &more);
+
+        static void setDelimiter(char newDelimiter);
+        static char getDelimiter();
+    };
 }
 #endif
