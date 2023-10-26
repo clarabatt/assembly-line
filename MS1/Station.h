52e91440a327a83b2ca534a9816b19cbf30bb5d2
Clara Battesini <clarabattesini@gmail.com>
2023-10-26 08:03:16 -0400

    feat: station class base header file

--- MS1/Station.h
+++ MS1/Station.h
@@ -15,5 +15,25 @@
 
 namespace sdds
 {
+    class Station
+    {
+        int m_id;
+        std::string m_name;
+        std::string m_description;
+        unsigned m_next_serial_num;
+        unsigned m_stock;
+
+        static unsigned m_widthField;
+        static unsigned id_generator;
+
+    public:
+        Station();
+        Station(std::string record);
+        const std::string &getItemName() const;
+        size_t getNextSerialNumber();
+        size_t getQuantity() const;
+        void updateQuantity();
+        void display(std::ostream &os, bool full) const;
+    }
 }
 #endif
