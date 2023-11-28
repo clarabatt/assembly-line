fdcc9be4a28204179714d4f8e0aefc762b64e27a
Clara Battesini <clarabattesini@gmail.com>
2023-11-28 13:51:58 -0500

    LineManager class header file

--- MS3/LineManager.h
+++ MS3/LineManager.h
@@ -11,13 +11,24 @@
 #define SDDS_LINE_MANAGER_H
 
 #include <iomanip>
+#include <vector>
 #include <iostream>
 #include "Utilities.h"
+#include "Workstation.h"
 
 namespace sdds
 {
     class LineManager
     {
+        std::vector<Workstation *> m_activeLine;
+        size_t m_cntCustomerOrder;
+        Workstation *m_firstStation;
+
+    public:
+        LineManager(const std::string &file, const std::vector<Workstation *> &stations);
+        void reorderStations();
+        bool run(std::ostream &os);
+        void display(std::ostream &os) const;
     };
 }
 
