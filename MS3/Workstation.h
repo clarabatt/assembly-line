c01da978f018bf0169826efa671a4ab1e20f5e20
Clara Battesini <clarabattesini@gmail.com>
2023-11-28 13:38:35 -0500

    base files for new modules

---
+++ MS3/Workstation.h
@@ -0,0 +1,24 @@
+// *****************************************************************************
+// Author  : Clara Verena Brito Battesini
+// Student ID(s)#: 143430213
+// Email        : cverena-brito-battes@senecacollege.ca
+// 28/11/2023
+//
+// I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+// *****************************************************************************
+
+#ifndef SDDS_WORKSTATION_H
+#define SDDS_WORKSTATION_H
+
+#include <iomanip>
+#include <iostream>
+#include "Utilities.h"
+
+namespace sdds
+{
+    class Workstation
+    {
+    };
+}
+
+#endif
