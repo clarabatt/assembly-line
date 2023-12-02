1ea46e102f91a925156c523fe90a7d1bbf086df0
Clara Battesini <clarabattesini@gmail.com>
2023-12-02 15:20:57 -0500

    Refactor LineManager's reorderStations method

--- MS3/LineManager.cpp
+++ MS3/LineManager.cpp
@@ -33,16 +33,20 @@ namespace sdds
                 std::string nextStationName = more ? util.extractToken(line, next_pos, more) : "";
 
                 auto currentIt = std::find_if(stations.begin(), stations.end(),
-                                              [&currentStationName](const Workstation* ws) { return ws->getItemName() == currentStationName; });
+                                              [&currentStationName](const Workstation *ws)
+                                              { return ws->getItemName() == currentStationName; });
 
                 auto nextIt = (!nextStationName.empty()) ? std::find_if(stations.begin(), stations.end(),
-                                                                        [&nextStationName](const Workstation* ws) { return ws->getItemName() == nextStationName; })
+                                                                        [&nextStationName](const Workstation *ws)
+                                                                        { return ws->getItemName() == nextStationName; })
                                                          : stations.end();
 
-                if (currentIt != stations.end()) {
-                    Workstation* currentStation = new Workstation((*currentIt)->generateStringBluePrint());
+                if (currentIt != stations.end())
+                {
+                    Workstation *currentStation = new Workstation((*currentIt)->generateStringBluePrint());
                     Workstation *nextStation = nullptr;
-                    if (nextIt != stations.end()) {
+                    if (nextIt != stations.end())
+                    {
                         nextStation = new Workstation((*nextIt)->generateStringBluePrint());
                     }
 
@@ -50,16 +54,16 @@ namespace sdds
                         currentStation->setNextStation(nextStation);
 
                     m_activeLine.push_back(currentStation);
-//                    if (!nextStationName.empty())
-//                        m_activeLine.push_back(nextStation);
+                    //                    if (!nextStationName.empty())
+                    //                        m_activeLine.push_back(nextStation);
 
-
-                    if (m_firstStation == nullptr) {
+                    if (m_firstStation == nullptr)
+                    {
                         m_firstStation = currentStation;
                     }
                 }
             }
-            catch (const std::invalid_argument& e)
+            catch (const std::invalid_argument &e)
             {
                 std::cerr << "Error in line " << line << " : " << e.what() << '\n';
             }
@@ -72,14 +76,48 @@ namespace sdds
 
     void LineManager::reorderStations()
     {
+        if (m_activeLine.empty())
+            return;
+
         std::vector<Workstation *> reordered;
         Workstation *currentStation = m_firstStation;
+        Workstation *nextStation = currentStation->getNextStation();
+        int counter = m_activeLine.size();
 
-        while (currentStation != nullptr)
+        reordered.push_back(currentStation);
+        m_activeLine.erase(std::find(m_activeLine.begin(), m_activeLine.end(), currentStation));
+
+        while (counter-- != 0 && nextStation != nullptr)
         {
-            reordered.push_back(currentStation);
-            currentStation = currentStation->getNextStation();
+            auto it = std::find_if(m_activeLine.begin(), m_activeLine.end(),
+                                   [&nextStation](Workstation *ws)
+                                   {
+                                       return ws->getItemName() == nextStation->getItemName();
+                                   });
+
+            if (it != m_activeLine.end())
+            {
+                nextStation = *it;
+                reordered.push_back(nextStation);
+                m_activeLine.erase(it);
+                currentStation = nextStation;
+                // std::cout << "1: " << currentStation->getItemName() << std::endl;
+                if (currentStation->getNextStation() != nullptr)
+                {
+                    nextStation = currentStation->getNextStation();
+                    // std::cout << "1: " << nextStation->getItemName() << std::endl;
+                }
+                else
+                {
+                    currentStation = m_activeLine.front();
+                    // std::cout << "2: " << currentStation->getItemName() << std::endl;
+                    if (currentStation->getNextStation() != nullptr)
+                        nextStation = currentStation->getNextStation();
+                    // std::cout << "2: " << nextStation->getItemName() << std::endl;
+                }
+            }
         }
+
         m_activeLine = std::move(reordered);
     };
 
